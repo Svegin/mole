4730dd5504ef681e84d29a81b998bc8c72676112
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-30 22:37:23 -0600

    update

--- master.js
+++ master.js
@@ -1,3 +1,30 @@
+const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
-const holes = document.querySelectorAll('.hole');
+let lastHole;
+let timeUp = false;
+
+function randomTime(max, min) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log('AHA same again');
+    return randomHole(holes);
+  }
+  lastHole =  hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() =>{
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
+}
